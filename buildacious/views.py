3d2379435f6a7d3be9454089347d015ca0d01c94
Leeroy Jenkins <jenkins@praekeltfoundation.org>
2012-07-24 14:46:53 +0200

    Buildacious

---
+++ buildacious/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import get_object_or_404, render_to_response
+from django.core.context_processors import csrf
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.utils import simplejson
+from django.contrib.auth.decorators import login_required
+
+from django import forms
+
+from lxml import etree
+
+import os
+
+from launchpadlib.launchpad import Launchpad
+
+
+def parseChanges(fn):
+    f = open(fn)
+
+    info = {'scfile': fn.split('/')[-1]}
+
+    for l in f:
+        if "Version:" in l and 'version' not in info:
+            info['version'] = l.split(':')[-1].strip()
+        if "Distribution:" in l:
+            info['dist'] = l.split(':')[-1].strip().capitalize()
+    return info
+
+@login_required
+def upload(request):
+    scf = request.get_full_path().split('/')[-1]
+    job = request.get_full_path().split('/')[-2]
+
+    f = open('/var/www/buildacious/uploads/'+scf, 'wt')
+    f.write('/var/lib/jenkins/jobs/%s/\n%s' % (job, scf))
+    f.close()
+
+    return HttpResponseRedirect('/')
+
+#@login_required
+def index(request):
+
+    stree = {}
+
+    jobs = os.listdir('/var/lib/jenkins/jobs/') 
+
+    lp = Launchpad.login_anonymously('Praekelt', 'production', '/tmp/lpcache')
+    ppa = lp.people['praekeltfoundation'].getPPAByName(name='ppa')
+    b = ppa.getPublishedBinaries()
+
+    ppastat = {}
+
+    for i in b:
+        n = "%s_%s_source.changes" % (i.binary_package_name, i.binary_package_version)
+        ppastat[n] = i.status
+
+    # Find sources
+    for j in jobs:
+        sources = [ i for i in 
+            os.listdir(os.path.join('/var/lib/jenkins/jobs/', j))
+            if 'source.changes' in i]
+
+        name = j.capitalize()
+        if sources: 
+            stree[name] = []
+            # Parse source.changes files 
+            for s in sources:
+                buildNum = s.split('+')[-1].split('-')[0]
+                info = parseChanges(os.path.join('/var/lib/jenkins/jobs/', j, s))
+
+                status = open(os.path.join('/var/lib/jenkins/jobs/', j,'builds',buildNum, 'build.xml')).read()
+                tree = etree.fromstring(status)
+                status = tree.xpath('//build/result')[0].text
+
+                upload = os.path.exists('/var/www/buildacious/uploads/'+s)
+                if upload:
+                    info['uploading'] = True
+
+                info['status'] = status
+
+                info['lpstatus'] = ppastat.get(s, None)
+                info['spath'] = j
+
+                stree[name].append(
+                    info
+                )
+        
+    print stree
+
+    rendata = {
+        'stree': stree
+    }
+    return render_to_response('index.html', rendata, context_instance=RequestContext(request))
---
+++ twisted/plugins/buildacious_plugin.py
@@ -0,0 +1,84 @@
+from zope.interface import implements
+
+from twisted.python import usage
+from twisted.plugin import IPlugin
+from twisted.application.service import IServiceMaker
+from twisted.application import internet
+from twisted.web import static, server, resource, client
+from twisted.internet import defer, reactor, threads, utils, task
+
+import json, os
+
+class Buildacious(resource.Resource):
+    isLeaf = True
+    addSlash = True
+
+    def __init__(self):
+        self.currentWorkers = {}
+        reactor.callWhenRunning(self.startCore)
+
+    def startCore(self):
+        self.task = task.LoopingCall(self.coreLoop)
+        self.task.start(5.0)
+
+    def endJob(self, res, job):
+        print job, res
+        out, err, signalNum = res
+        print "Job end", out
+        os.remove(os.path.join('/var/www/buildacious/uploads', job))
+        del self.currentWorkers[job]
+
+    def coreLoop(self):
+        tasks = os.listdir('/var/www/buildacious/uploads')
+
+        for t in tasks:
+            if t not in self.currentWorkers:
+                # Spawn the upload
+                job, sc = open('/var/www/buildacious/uploads/'+t).read().split('\n')
+
+                print "Spawning", job, sc
+                self.currentWorkers[t] = utils.getProcessOutputAndValue(
+                    '/usr/bin/dput', args=('ppa:praekeltfoundation/ppa', os.path.join(job,sc)), path=job
+                ).addBoth(self.endJob, t)
+
+        if self.currentWorkers:
+            print "Current workers:", self.currentWorkers
+
+
+    def completeCall(self, response, request):
+        # Render the json response from call
+        response = json.dumps(response)
+        request.write(response)
+        request.finish()
+
+    def render_GET(self, request):
+        request.setHeader("content-type", "application/json")
+        
+        print dir(request)
+
+        return "buildacious"
+
+    def render_POST(self, request):
+        request.setHeader("content-type", "application/json")
+        # Get request 
+        command = cgi.escape(request.args["command"][0])
+        params = json.loads(cgi.escape(request.args["params"][0]))
+
+        d.addCallback(self.completeCall, request)
+
+        return server.NOT_DONE_YET
+
+class Options(usage.Options):
+    optParameters = [["port", "p", 6012, "The port to listen on."]]
+
+class MyServiceMaker(object):
+    implements(IServiceMaker, IPlugin)
+    tapname = "buildacious"
+    description = "Buildacious bot"
+    options = Options
+
+    def makeService(self, options):
+        return internet.TCPServer(int(options["port"]), server.Site(Buildacious()))
+
+
+serviceMaker = MyServiceMaker()
